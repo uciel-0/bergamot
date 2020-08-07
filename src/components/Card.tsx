a5a8a9697e0fbba8b036ad404081c338e3020152
uvilchis <uciel.vilchis7@gmail.com>
2020-08-07 01:23:05 -0400

    updates HTML for card - shows prices on the right side

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,18 +3,26 @@ import * as React from 'react'
 // also include logic to do a custom calculation for fees (in case the price after fees is not avaialble)
 // stubhub has a field called ticketInfo -> if ticketInfo.totalListings = 0, show some sort of message indicating that fact
 export const Card = ({date, priceBeforeFees, priceAfterFees, source, name}: any) => {
-  const priceBeforeFeesText = priceBeforeFees ? `Price before fees: $${priceBeforeFees}` : null;
-  const priceAfterFeesText = priceAfterFees ? `Price after fees: $${priceAfterFees}` : null;
+  const priceBeforeFeesText = "Price before fees: ";
+  const priceAfterFeesText = "Price after fees: ";
+  const priceBeforeFeesFormatted = priceBeforeFees ? '$'+ priceBeforeFees + '.00' : '';
+  const priceAfterFeesFormatted = priceAfterFees ? '$' + priceAfterFees + '.00' : '';
+  const priceBeforeFeesHTML = priceBeforeFees ? <p className="Card_prices-text">{priceBeforeFeesText}<b className="Card_prices-soft">{priceBeforeFeesFormatted}</b></p> : null;
+  const priceAfterFeesHTML = priceAfterFees ?  <p className="Card_prices-text">{priceAfterFeesText}<b className="Card_prices-bold">{priceAfterFeesFormatted}</b></p> : null;
   return (
     <div className="Card">
-      <span className="Card_logo">
-        <SourceLogo source={source}/>
-      </span>
-      <div className="Card_info">
-        <p>{name}</p>
-        <p>{date}</p>
-        <p>{priceBeforeFeesText}</p>
-        <p>{priceAfterFeesText}</p>
+      <div className="Card_content">
+        <span className="Card_logo">
+          <SourceLogo source={source}/>
+        </span>
+        <div className="Card_info">
+          <p>{name}</p>
+          <p>{date}</p>
+        </div>
+        <div className="Card_prices">
+          {priceBeforeFeesHTML}
+          {priceAfterFeesHTML}
+        </div>
       </div>
     </div>
   )

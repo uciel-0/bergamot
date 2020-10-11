9b4b1b177889d8e6ffc708794de52ab436e51977
Iblasio Sanchez <Iblasio.sanchez@gmail.com>
2020-10-11 18:17:57 -0400

    added images to home

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -3,5 +3,9 @@ import * as React from 'react';
 export const Home = () => (
     <div className="Home">
       <h1 className="Home_title">Home</h1>
+        <div className="images d-inline-flex">
+          <img src="\bop-photo-1.png"></img>
+          <img src="\bop-photo-2.png"></img>
+         </div>
     </div>
 )

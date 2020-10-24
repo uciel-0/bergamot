e278b5d76ddd0ff2488b7cdf5a5f00ef4518a623
uvilchis <uciel.vilchis7@gmail.com>
2020-10-24 02:57:55 -0400

    adds basleine work for cache plus server side comments

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -2,6 +2,7 @@ import {getTicketMasterSearchResults} from './ticketmaster';
 import {getSeatGeekSearchResults, getSeatGeekEvents} from './seatgeek';
 import {getStubhubSearchResults, getStubhubEvents} from './stubhub'
 import {groupByDay, formatDate, formatLocalDate, formatTime} from '../utils/dateUtils';
+import Cache from '../cache';
 // final modifications to the array we recieve on the front-end are made here 
 export const wideSearchResults = (req, res) => {
   const ticketmaster = getTicketMasterSearchResults(req, res);
@@ -55,15 +56,21 @@ export const wideSearchResults = (req, res) => {
     console.log('error in master search', err);
     res.sendStatus(400);
   });
-}
+}  
+// initialize the cache here
+const ttl = 60 * 60 * 1;
+const cache = new Cache(ttl);
 // this is the api call we're using
 export const getEvents = (req, res) => {
   const ticketmaster = getTicketMasterSearchResults(req, res);
   const stubhub = getStubhubEvents(req, res);
   const seatgeek = getSeatGeekEvents(req, res); 
-  return Promise.all([ticketmaster, stubhub, seatgeek])
+  // create the key under which this data will be stored 
+  const key = `getEvents_${req.query.keyword}`;
+  // when the response is complete, store it in the cache 
+  return cache.get(key, () => Promise.all([ticketmaster, stubhub, seatgeek]))
   .then(data => {
-    // set some custom fields for easy front end access
+    // Step 1) Set some custom fields for easy front end access
     // ticketmaster events
     data[0].events.map(e => {
       e.source = 'ticketmaster';
@@ -112,9 +119,9 @@ export const getEvents = (req, res) => {
       e.priceAfterFees = e.stats.lowest_price;
       e.isPriceEstimated = false;
     });
-    // process for combining the data, sorting then grouping it by date
+    // Step 2) Combinine the data into one set
     const combinedData = [...data[0].events, ...data[1].events, ...data[2].events];
-    // finds the minimum and maximum values from the whole data set
+    // Step 3) Find the minimum and maximum values from the whole data set
     const minMax = combinedData.reduce((accumulator, currentValue) => {
       const minPrice = currentValue.priceAfterFees ? Math.min(currentValue.priceAfterFees, accumulator[0]) :  Math.min(Number.POSITIVE_INFINITY, accumulator[0]);
       const maxPrice = currentValue.priceAfterFees ? Math.max(currentValue.priceAfterFees, accumulator[1]) : Math.max(Number.NEGATIVE_INFINITY, accumulator[1]);
@@ -123,19 +130,21 @@ export const getEvents = (req, res) => {
         Math.round(maxPrice)
       ]
     }, [Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY]); 
-    // include a function to find the min
+    // Step 4) Sort the data chronologically
     const sortChronologically = combinedData.sort((a, b) => new Date(a.date) - new Date(b.date));
+    // Step 5a) Group the chronologically sorted data by date
     const groupedData = groupByDay(sortChronologically);
-    // data with an unknown date ends up sorted into a null bucket by default
-    // this line makes it so that null bucket is the last item, so it will show up last in the front end
+    // Data with an unknown date ends up sorted into a null bucket by default
+    // Step 5b) This chunk makes it so that null bucket is the last item, so it will show up last in the front end
     if (groupedData[0].date === 'null') {
       const datesTBD = groupedData.shift();
       groupedData.push(datesTBD);
     };
-    // if any of these results sets have a length of zero, we'll let the front end know via boolean
+    // Step 6) Determine which distributor returned data in this query
     const hasTicketmasterData = data[0].events.length > 0;
     const hasStubhubData = data[1].events.length > 0;
     const hasSeatgeekData = data[2].events.length > 0;
+    // Step 7) Arrange the data together
     const response = {
       data: groupedData,
       source: {
@@ -146,6 +155,7 @@ export const getEvents = (req, res) => {
       minPrice: minMax[0],
       maxPrice: minMax[1]
     }
+    // Step 8) Send the data
     res.send(response);
   })
   .catch((err) => {

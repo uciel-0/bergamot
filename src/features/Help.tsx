f8e305502031857728197567362ab2872f4d9307
Iblasio Sanchez <Iblasio.sanchez@gmail.com>
2022-02-10 20:32:56 -0500

    review before merging

---
+++ src/features/Help.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const ErrorScreen = () => {
+  return (
+      <div className="Error">
+        <h1 className="Error_message">We ain't find jack shit. You good homie?</h1>    
+      </div>
+  )
+}

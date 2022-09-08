22a233890b69cc48401b17217df597e15022d68c
f648089 <uciel.vilchis@jpmchase.com>
2022-09-08 15:54:22 -0400

    adds setup-instance.sh script

---
+++ setup-instance.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
+
+curl -o- -L https://yarnpkg.com/install.sh | bash

1de5f31480f7bf4dc9d1dbdff0fc184eb47520ad
f648089 <uciel.vilchis@jpmchase.com>
2022-09-09 11:16:32 -0400

    more script attempts

--- setup-instance.sh
+++ setup-instance.sh
@@ -2,11 +2,9 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "/root/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
-
-export PATH="/usr/local/share/npm/bin:/root/.nvm/versions/node/v16.17.0"
 
 npm i yarn -g

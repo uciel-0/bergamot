d233480d9ed3a0e7de17655b4b29c2d6f9c50201
f648089 <uciel.vilchis@jpmchase.com>
2022-09-09 10:11:45 -0400

    updates setup scripts

--- setup-instance.sh
+++ setup-instance.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "/root/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
 

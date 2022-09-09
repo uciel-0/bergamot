4579723dd8b400a4df537320b1b47e8931d41033
f648089 <uciel.vilchis@jpmchase.com>
2022-09-09 10:03:44 -0400

    sets up proper path for node

--- setup-instance.sh
+++ setup-instance.sh
@@ -4,4 +4,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
 
-curl -o- -L https://yarnpkg.com/install.sh | bash
+export PATH="/usr/local/share/npm/bin:/root/.nvm/versions/node/v16.17.0"
+
+npm i yarn -g

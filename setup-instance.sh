0e418f1ea7653fb99ce4063f9f2f981c8002e68c
uvilchis <uciel.vilchis7@gmail.com>
2022-09-09 23:37:24 -0400

    tries another script

--- setup-instance.sh
+++ setup-instance.sh
@@ -1,11 +1,47 @@
 source /root/.bash_profile
+# References
+# -------------
+# https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html#w2ab1c23b7c10
+# https://gist.github.com/sealocal/0cd468ba4f12cdada436aebe534b40da
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install --lts
+set -xe
 
-curl -o- -L https://yarnpkg.com/install.sh | bash
-yarn install
+NODE_VERSION=v9.3.0
+YARN_REPO="/etc/yum.repos.d/yarn.repo"
+
+# If yarn is not detected, install it.
+if which yarn; then
+  echo "Skipping installation of yarn -- yarn already installed."
+  echo "yarn --version: `yarn --version`"
+else
+  echo "which yarn: `which yarn`"
+  echo "Yarn is not installed and accessible."
+  echo "Installing yarn..."
+
+  if [ ! -f $YARN_REPO ]; then
+    sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O $YARN_REPO
+  
+    # Confirm that it downloaded
+    file /etc/yum.repos.d/yarn.repo
+  fi
+  
+  # If node is not detected, install it.
+  if [ `node --version` == "v$NODE_VERSION" ]; then
+    echo "Skipping installation of node -- node already installed."
+    echo "node --version: `node --version`"
+  else
+    echo "Installing Node $NODE_VERSION"
+
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
+    source ~/.nvm/nvm.sh
+
+    nvm install $NODE_VERSION
+  fi
+
+  # install yarn
+  sudo yum install -y yarn
+  yarn --version
+
+  echo "... and finished installing yarn."
+fi

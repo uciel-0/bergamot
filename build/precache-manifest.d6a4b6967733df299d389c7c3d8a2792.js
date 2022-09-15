c7883e7c9807db926d00c9471eae05a73977230f
f648089 <uciel.vilchis@jpmchase.com>
2022-09-15 11:32:49 -0400

    adds build files

---
+++ build/precache-manifest.d6a4b6967733df299d389c7c3d8a2792.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71edb438dccdfa72d93f14b0bdbe942a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2769e6a5c06d84f2699e",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "610f7736632296094d17",
+    "url": "/static/css/main.7c380095.chunk.css"
+  },
+  {
+    "revision": "2769e6a5c06d84f2699e",
+    "url": "/static/js/2.e40b75a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e40b75a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610f7736632296094d17",
+    "url": "/static/js/main.cef8ed3e.chunk.js"
+  },
+  {
+    "revision": "99b261706c75964aa497",
+    "url": "/static/js/runtime-main.b4efd4ae.js"
+  },
+  {
+    "revision": "5b7c4c940e2f6105c75250e5f7ea4670",
+    "url": "/static/media/SG_Logo_Side_Blue.5b7c4c94.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
